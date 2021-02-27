53ca8c3838a1775a6fc25a017d973e467223e5fa
nbornson26 <nbronson26@gmail.com>
2021-02-26 19:16:55 -0800

    daily challenge

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let myWord = 'Supercalifragilisticexpialidocious';
+console.log(myWord.length);
+
+let myNumber = 8;
+console.log(myNumber);
+
+const bigint = 1234567890123456789012345678901234567890n;
+console.log(bigint);
+
+console.log(typeof myNumber);
+console.log(typeof bigint);
